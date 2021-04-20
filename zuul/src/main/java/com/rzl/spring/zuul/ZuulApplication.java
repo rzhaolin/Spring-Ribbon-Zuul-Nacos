06f6b66b0daea3f86750bf9976d777b4ac835ad5
rizhao.lin <rizhao.lin@baidao.com>
2021-04-20 23:39:22 +0800

    去掉部分代码

--- zuul/src/main/java/com/rzl/spring/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/rzl/spring/zuul/ZuulApplication.java
@@ -6,12 +6,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableZuulProxy
+@EnableDiscoveryClient // 允许注册nacos
+@EnableZuulProxy  // 开启 zuul代理
 public class ZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
 	}
-
 }
